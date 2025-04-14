41ee1fdf4cb8a5006cfcdc703c150549332e31d0
lipewz <felipecosta.augusto2008@gmail.com>
2025-04-14 08:32:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var paises = ['Argentina', 'Canada', 'Uruguai', 'Azerbaijão', 'Ilhas Salomão', 'Brasil','Estados Unidos', 'irã', 'Peru','Angola','Equador']
+var paisEscolhido = parseFloat(prompt("Digite um país de 0 a 10"))
+document.write("País escolhido: " + paises[paisEscolhido])
+
+document.write("<br>")
+
+var cidades = ['Arroio dos Ratos', 'Rolândia', 'Guzolândia', 'Curralinho', 'Jardim de Piranhas', 'Anta gorda','Pintópolis', 'Capimpuba', 'Passa e fica','Entrepelado','Fruta de Leite', 'Verdelândia', 'Xique-xique', 'Barro Duro', 'São Miguel do Gostoso', 'Monte Formoso']
+var cidadeEscolhida = parseFloat(prompt("Digite uma cidade de 0 a 15"))
+document.write("Cidade escolhida: " + cidades[cidadeEscolhida])
+
+document.write("<br>")
+
+var nomes = ['João', 'Maria', 'Pedro', 'Ana', 'Carlos', 'Julia', 'Felipe', 'Rafaela', 'Gabriel', 'Luciana', 'Thiago', 'Fernanda', 'Eduardo', 'Carla', 'Vinícius', 'Patrícia', 'Ricardo', 'Simone', 'Lucas', 'Cláudia', 'André', 'Mariana', 'Rodrigo', 'Isabela', 'Leandro', 'Raquel', 'Bruno', 'Vanessa', 'Alexandre', 'Lúcia']
+var nomeEscolhido = parseFloat(prompt("Digite um nome de 0 a 30"))
+document.write("Nome escolhido:" + nomes[nomeEscolhido])
